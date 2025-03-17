9268a2ac0d8364bb0d3dca7b2b8bb1dcd256dc1f
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-03-17 13:10:22 +0300

    Initial Commit

---
+++ APICall/APICallApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICallApp.swift
+//  APICall
+//
+//  Created by Abdullah Hafiz on 17/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct APICallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
